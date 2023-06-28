f1aa6110e3a3e6d85e20d391d6c641f280d408d4
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-28 14:31:31 +0530

    .

--- Oxygenvalue.cpp
+++ Oxygenvalue.cpp
@@ -11,15 +11,37 @@ while(count<=9){
     if (x>=1 && x<=100){
         
         if (count%3==1){
-            T1 = T1+x;
+            T1 = T1 + x;
         }
         else if (count%3==2){
-            T2 = T2+x;
+            T2 = T2 + x;
         }
         else{
-            T3 = T3+x;
+            T3 = T3 + x;
+        }
+        count++;
+    }
+        else
+        {
+            cout << "INVALID INPUT";
+            count++;
+            return 0;
         }
     }
+    A1 = round(T1/3);
+    A2 = round(T2/3);
+    A3 = round(T3/3);
+    if(A1 <= 70 && A2 <= 70 && A3 <= 70)
+    {
+        cout << "All trainees are unfit";
+        return 0;
+    }
+    if(A1 >= A2 && A1>= A3)
+        cout << "Trainee Number: 1"<< endl;
+    if(A2 >= A1 && A2 >= A3)
+        cout << "Trainee Number: 2"<< endl;
+    if(A3 >= A1 && A3 >= A2)
+        cout << "Trainee Number: 3" << endl;
+    return 0;
+}
 
-}
-}
