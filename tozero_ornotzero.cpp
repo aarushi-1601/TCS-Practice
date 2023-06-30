cce66a7f1a58a384a37bb7437ba7b518a8ca709a
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-30 14:05:32 +0530

    .

--- tozero_ornotzero.cpp
+++ tozero_ornotzero.cpp
@@ -5,12 +5,13 @@ int main(){
     int m,n;
     cin >> n >> m;
     for(int i=m; i<=n; i++){
-        if m >=100;
-        printf("%03d", i);
-        else if m>=10;
-        printf("%02d", i);
-        else;
-        printf("%0d", i);
+        if (m >=100)
+              printf("%03d", i);
+        else if (m >=10)
+              printf("%02d", i);
+        else
+              printf("%d", i);
 
     }
+    return 0;
 }
